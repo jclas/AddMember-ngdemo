0e96c89d8b055ee678971678c7bb98476a8303cb
John <johnc_mtr@hotmail.com>
2025-12-31 14:12:23 -0600

    - For Edit pages, moved display name to page header. - rearranged order of some entry fields to make more sense. - made the app header into a link that returns to starting page. - shrunk smaller entry fields to 200px width. - made edit form headers more consistant with add form headers.

--- member-add/src/app/app.component.ts
+++ member-add/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
